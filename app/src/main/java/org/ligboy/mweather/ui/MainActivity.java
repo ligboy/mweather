77f07c594a37f1fcdbc471f0c1ba1115587f578b
ligboy <Ligboy@gmail.com>
2016-07-02 17:31:01 +0800

    Scrollable Toolbar

--- app/src/main/java/org/ligboy/mweather/ui/MainActivity.java
+++ app/src/main/java/org/ligboy/mweather/ui/MainActivity.java
@@ -5,9 +5,7 @@ import android.os.Build;
 import android.os.Bundle;
 import android.support.constraint.ConstraintLayout;
 import android.support.design.widget.CoordinatorLayout;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
-import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.view.ViewPager;
@@ -17,7 +15,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 import org.ligboy.mweather.R;
 import org.ligboy.mweather.adapter.MainPageAdapter;
@@ -25,7 +22,7 @@ import org.ligboy.mweather.adapter.MainPageAdapter;
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
-    private ConstraintLayout mContentLayout;
+    private CoordinatorLayout mContentLayout;
     private ViewPager mViewPager;
     private TabLayout mTabLayout;
 
@@ -36,7 +33,7 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mContentLayout = (ConstraintLayout) findViewById(R.id.constraintLayout);
+        mContentLayout = (CoordinatorLayout) findViewById(R.id.coordinatorLayout);
         mViewPager = (ViewPager) findViewById(R.id.viewPager);
         mTabLayout = (TabLayout) findViewById(R.id.tabLayout);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
